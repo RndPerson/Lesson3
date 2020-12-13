d4b72d8fd3350c02454347613a9a81363d5424fb
shekarno <kirill_45@yahoo.com>
2020-12-13 16:20:57 +0500

    lesson 3 done

--- Lesson3/Controllers/SecondViewController.swift
+++ Lesson3/Controllers/SecondViewController.swift
@@ -9,4 +9,89 @@ import UIKit
 
 class SecondViewController : UIViewController {
     
+    var cnt: CGFloat = 0
+    
+    private var lastTranslation: CGFloat = 0
+    
+    let numLabel: UILabel = {
+    let label = UILabel()
+        label.text = "0"
+        label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+    }()
+    
+    let touchView: UIView = {
+        let touch = UIView()
+        touch.backgroundColor = .green
+        touch.translatesAutoresizingMaskIntoConstraints = false
+        return touch
+    }()
+    
+    private lazy var panGestureRecognizer: UIPanGestureRecognizer = UIPanGestureRecognizer(
+        target: self,
+        action: #selector(touchViewDidSlide(_:))
+    )
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(numLabel)
+        view.addSubview(touchView)
+        setupNumLabel()
+        setupTouchView()
+        touchView.addGestureRecognizer(panGestureRecognizer)
+    }
+    
+    func setupNumLabel() {
+        
+        numLabel.topAnchor.constraint(
+            equalTo: view.topAnchor,
+            constant: 30
+       ).isActive = true
+        
+        numLabel.centerXAnchor.constraint(
+            equalTo: view.centerXAnchor
+        ).isActive = true
+        
+        numLabel.heightAnchor.constraint(
+            equalToConstant: 30
+        ).isActive = true
+        
+    }
+    
+    func setupTouchView() {
+        
+       touchView.topAnchor.constraint(
+            equalTo: numLabel.bottomAnchor,
+            constant: 40
+       ).isActive = true
+        
+        touchView.centerXAnchor.constraint(
+            equalTo: view.centerXAnchor
+        ).isActive = true
+        
+        touchView.heightAnchor.constraint(
+            equalToConstant: 200
+        ).isActive = true
+        
+        touchView.widthAnchor.constraint(
+            equalToConstant: 300
+        ).isActive = true
+    }
+    
+    @objc func touchViewDidSlide(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            print("began")
+            
+        case .changed:
+            cnt = lastTranslation - sender.translation(in: touchView).y
+            
+        case .ended:
+            lastTranslation = sender.translation(in: touchView).y
+            numLabel.text = "\(cnt)"
+            
+        default: break
+        }
+    }
 }
